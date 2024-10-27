ab4dc75f52517567b278c086d43173cdfdbc87b4
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-10-28 00:21:41 +0900

    feat : 수정

--- src/components/LogCard.jsx
+++ src/components/LogCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../css/components/LogCard.css';
+import image from "../img/image.png";
 
 export default function LogCard() {
     return(
@@ -11,7 +12,7 @@ export default function LogCard() {
                 </div>
                 <div className="date">2024.10.10</div>
             </div>
-            <img className="LogCardImage" src="" alt="Log Thumbnail"/>
+            <img className="LogCardImage" src={image} alt="Log Thumbnail"/>
             <div className="Title">Demo title</div>
             <div className="Preview">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</div>
         </div>
