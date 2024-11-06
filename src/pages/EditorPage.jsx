5a0637d8668ac3859eec95883ddabecb75714c03
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-11-07 00:30:19 +0900

    toast-ui 11/7

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,6 +1,17 @@
 import { Editor } from "@toast-ui/react-editor";
+import '@toast-ui/editor/dist/toastui-editor.css';
+import styled from "styled-components";
+
+const EditorWrapper = styled.div`
+    min-height: 100%;
+    padding: 10%;
+`;
 
 export default function EditorPage() {
 
-    return(<Editor />);
-}
+    return(
+        <EditorWrapper>
+            <Editor />
+        </EditorWrapper>
+    );
+} 
